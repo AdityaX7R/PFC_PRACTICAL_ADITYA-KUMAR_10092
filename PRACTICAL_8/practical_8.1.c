bd73337cf1d57769ef202f7be982be27f30b06a0
Aditya Kumar <adikr4477@gmail.com>
2025-12-23 09:32:40 +0000

    sum of two digit using function

---
+++ PRACTICAL_8/practical_8.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int sumOfDigit(int n){
+    int output=0;
+    while(n!=0){
+        int ld=n%10;
+        output+=ld;
+        n=n/10;
+    }
+    return output;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int ans=sumOfDigit(n);
+    printf("%d",ans);
+}
