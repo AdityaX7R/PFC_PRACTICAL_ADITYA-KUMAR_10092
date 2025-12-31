7c97e9f2ba1f4bfbceac75bf7fd4aee7592e50c0
Aditya Kumar <adikr4477@gmail.com>
2025-12-31 10:25:52 +0530

    Add program to find max and min in 2D array

---
+++ PRACTICAL_10/practical_10.1.c
@@ -0,0 +1,36 @@
+// Max and min element in 2D array
+#include <stdio.h>
+
+int main() {
+  // Aditya Kumar 10092
+    int arr[3][3] = {
+        {5, 8, 2},
+        {9, 3, 6},
+        {4, 7, 1}
+    };
+
+    int max = arr[0][0];
+    int min = arr[0][0];
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(arr[i][j] > max)
+                max = arr[i][j];
+            if(arr[i][j] < min)
+                min = arr[i][j];
+        }
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
+
+
+
+// output
+
+
+// Maximum element = 9
+// Minimum element = 1
